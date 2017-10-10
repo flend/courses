eda9c5f9099a6bd13dfd1069bc243c4f74d51d97
flend <flend@compsoc.net>
2017-10-10 22:24:46 +0100

    Adding explanation to dcgan

--- dungeonlearning/dcgan.py
+++ dungeonlearning/dcgan.py
@@ -4,6 +4,7 @@ import torch.nn.parallel
 import torch.utils.data as tdata
 
 class DCGAN_D(nn.Module):
+    # input channels (1), output channels, kernel_size, stride, padding
     def conv_block(self, main, name, inf, of, a, b, c, bn=True):
         convName = str.format("{}-{}.{}.conv", name, inf, of)
         main.add_module(convName, nn.Conv2d(inf, of, a, b, c, bias=False))
@@ -12,6 +13,7 @@ class DCGAN_D(nn.Module):
         reluName = str.format("{}-{}.relu", name, of)
         main.add_module(reluName, nn.LeakyReLU(0.2, inplace=True))
 
+        #netD = DCGAN_D(sz, 1, 64, 1, 1).cuda()
     def __init__(self, isize, nc, ndf, ngpu, n_extra_layers=0):
         super(DCGAN_D, self).__init__()
         self.ngpu = ngpu
@@ -24,14 +26,20 @@ class DCGAN_D(nn.Module):
 
         for t in range(n_extra_layers):
             extraName = str.format("extra-{}", t)
+            # input and output of ndf (64 channels), size 3 kernel
+            # no decrease in image size
             self.conv_block(main, extraName, cndf, cndf, 3, 1, 1)
 
         while csize > 4:
+            # 4-size kernel, 2 stride, reduces size by factor of 2 (apparently)
+            # continue until size is 4 (must have been multiple of 16)
             self.conv_block(main, 'pyramid', cndf, cndf*2, 4, 2, 1)
             cndf *= 2; csize /= 2
 
         # state size. K x 4 x 4
         cndfName = str.format("final.{}-1.conv", cndf)
+        # Output to single channel, 4x4 for 0/1 discrimination
+        # mean occurs in output
         main.add_module(cndfName, nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
         self.main = main
 
@@ -53,27 +61,34 @@ class DCGAN_G(nn.Module):
         reluName = str.format("{}-{}.relu", name, of)
         main.add_module(reluName, nn.ReLU(inplace=True))
 
+        #netG = DCGAN_G(sz, nz, 1, 64, 1, 1).cuda()
     def __init__(self, isize, nz, nc, ngf, ngpu, n_extra_layers=0):
         super(DCGAN_G, self).__init__()
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
+        #cngf = 32 * 16 (sz / 4) = 512
         cngf, tisize = ngf//2, 4
         while tisize != isize: cngf *= 2; tisize *= 2
 
         main = nn.Sequential()
+        # kernel size 4 deconv on original noise
         self.deconv_block(main, 'initial', nz, cngf, 4, 1, 0)
 
         csize, cndf = 4, cngf
         while csize < isize//2:
+            # Reduce filters by half, double size of image, until half original size
+            # Perform 5 times
             self.deconv_block(main, 'pyramid', cngf, cngf//2, 4, 2, 1)
             cngf //= 2; csize *= 2
 
         for t in range(n_extra_layers):
             extraName = str.format("extra-{}", t)
+            # Extra deconv, does not affect size
             self.deconv_block(main, extraName, cngf, cngf, 3, 1, 1)
 
         cnfgName = str.format("final.{}-{}.convt", cngf, nc)
+        # Final deconv to full sized image
         main.add_module(cnfgName, nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
         ncName = str.format("final.{}.sigmoid", nc)
         main.add_module(ncName, nn.Sigmoid())
